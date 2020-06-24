fade5a74e3c8786c4c7b66fffd193173cb40aacb
Patrick Freinschlag <patrick.freinschlag@gmail.com>
2020-06-24 11:56:16 +0200

    finishes prework

---
+++ petStore/src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: 'cat',
+    price: 799,
+    description: 'lorem ipsum'
+  },
+  {
+    name: 'dog',
+    price: 699,
+    description: 'lorem ipsum'
+  },
+  {
+    name: 'bunny',
+    price: 299,
+    description: 'lorem ipsum'
+  }
+ ];
